02b1cfb4cf76b198a08461aed4e7e3924197add6
ofekhadjadj <ofek.haghag@gmail.com>
2024-02-29 22:36:42 +0200

    before finish the zoo page need to fix bug with save clicked animal after filtering

--- zoo.js
+++ zoo.js
@@ -1,4 +1,7 @@
 let animalsArry = JSON.parse(localStorage.getItem("animals")); // תפיסת מערך חיות מלוקל סטורג
+//תפיסת אלמנטים
+let animalCard = document.getElementById("animal-cards");
+
 const searchAnimalName = document.getElementById("name-search");
 const searchAnimalWeight = document.getElementById("weight-search");
 const searchAnimalHeight = document.getElementById("height-search");
@@ -9,43 +12,42 @@ const pradatorSearchTrue = document.getElementById("is-pradator");
 const pradatorSearchFalse = document.getElementById("not-pradator");
 const clearBt = document.getElementById("clear-bt");
 
+//אוביקט של פורם לצורך שמירה בלוקל סטורג ואיפוס
+let formObj = {
+  nameForm: "",
+  WeightForm: "",
+  HeightForm: "",
+  colorForm: "",
+  habitatLandForm: false,
+  habitatSeaForm: false,
+  pradatorTrueForm: false,
+  pradatorFalseForm: false,
+};
+
+//בדיקה אם קיים מפתח בשם זה בלוקל סטורג, אם לא קיים נפתח אחד כזה ונשים בו את מערך החיות
 if (JSON.parse(localStorage.getItem("filterd array"))) {
 } else {
   localStorage.setItem("filterd array", JSON.stringify(animalsArry));
 }
 
-// colorSearch.addEventListener("change", () => console.log(colorSearch.value));
+renderAvailableAnimals(); //מקים בדף את כרטיסי כל החיות
+//תפיסת כל הכרטיסים הקיימים למערך 1
+// האזנות לאירועים בפורם
+searchAnimalName.addEventListener("input", filtering);
+searchAnimalWeight.addEventListener("input", filtering);
+searchAnimalHeight.addEventListener("input", filtering);
+colorSearch.addEventListener("input", filtering);
+habitatSearchLand.addEventListener("input", filtering);
+habitatSearchSea.addEventListener("input", filtering);
+pradatorSearchTrue.addEventListener("input", filtering);
+pradatorSearchFalse.addEventListener("input", filtering);
+clearBt.addEventListener("click", clearForm);
 
-// function arryFilteringName(e) {
-//   //פונקצייה שמופעלת על ידי האיוונט ליסטינר
-//   // cardDiv.innerHTML = ``; //איפוס הדיב של המבקרים
-//   let newArry = animalsArry.filter(function (animal) {
-//     // יצירת מערך חדש שיכיל את המערך המסונן
-//     return animal.name.toLowerCase().includes(e.target.value.toLowerCase()); //בדיקת הכלה של האותיות שהוזנו והמרה לאותיות קטנות
-//   });
-//   localStorage.setItem("filterd array", JSON.stringify(newArry));
-//   // loadVisitors(newArry); //רנדור מערך מסונן
-// }
-
-//האזנה לאירוע שינוי באינפוט
-// function arryFilteringWeight(e) {
-//   //פונקצייה שמופעלת על ידי האיוונט ליסטינר
-//   // cardDiv.innerHTML = ``; //איפוס הדיב של המבקרים
-//   let newArry = animalsArry.filter(function (animal) {
-//     // יצירת מערך חדש שיכיל את המערך המסונן
-//     return animal.weight > e.target.value;
-//   });
-//   localStorage.setItem("filterd array", JSON.stringify(newArry));
-//   // loadVisitors(newArry); //רנדור מערך מסונן
-// }
-
-//בדיקה של פונקציה
-searchAnimalName.addEventListener("input", filterTest);
-searchAnimalWeight.addEventListener("input", filterTest);
-
-function filterTest(e) {
+//פונקציה שמשנה את המערך בלוקל סטורג לפי המסננים
+function filtering(e) {
   let newArry = animalsArry;
   if (searchAnimalName.value) {
+    //בדיקת מסנן שם
     newArry = newArry.filter(function (animal) {
       return animal.name
         .toLowerCase()
@@ -53,35 +55,114 @@ function filterTest(e) {
     });
   }
   if (searchAnimalWeight.value) {
-    console.log(searchAnimalWeight.value);
     newArry = newArry.filter(function (animal) {
       return animal.weight > searchAnimalWeight.value;
     });
-    console.table(newArry);
   }
-  localStorage.setItem("filterd array", JSON.stringify(newArry));
+
+  if (searchAnimalHeight.value) {
+    newArry = newArry.filter(function (animal) {
+      return animal.height > searchAnimalHeight.value;
+    });
+  }
+
+  if (colorSearch.value) {
+    newArry = newArry.filter(function (animal) {
+      return animal.color === colorSearch.value;
+    });
+  }
+
+  if (habitatSearchLand.checked || habitatSearchSea.checked) {
+    newArry = newArry.filter(function (animal) {
+      return habitatSearchLand.checked
+        ? animal.habitat === habitatSearchLand.value
+        : animal.habitat === habitatSearchSea.value;
+    });
+  }
+
+  if (pradatorSearchTrue.checked || pradatorSearchFalse.checked) {
+    newArry = newArry.filter(function (animal) {
+      return pradatorSearchTrue.checked
+        ? animal.isPredator.toString() == pradatorSearchTrue.value
+        : animal.isPredator.toString() == pradatorSearchFalse.value;
+    });
+  }
+
+  formObjUpdate(); //הפעלת פונקצית עדכון אוביקט הטופס
+
+  localStorage.setItem("filterd array", JSON.stringify(newArry)); //שמירת המערך המסונן בלוקל סטורג
+  renderAvailableAnimals(); //הצגת הכרטיסים של החיות בדף
 }
 
+//פונקציה שמאפסת את המסננים בטופס
+function clearForm() {
+  searchAnimalName.value = "";
+  searchAnimalWeight.value = "";
+  searchAnimalHeight.value = "";
+  colorSearch.value = "";
+  habitatSearchLand.checked = false;
+  habitatSearchSea.checked = false;
+  pradatorSearchFalse.checked = false;
+  pradatorSearchTrue.checked = false;
+  formObjClear(); //הפעלת פונקצית ניקוי לטופס
+
+  localStorage.setItem("filterd array", JSON.stringify(animals)); //שמירת המערך המקורי במקום המערך במסונן בלוקל סטורג
+  renderAvailableAnimals(); //הצגת הכרטיסים של החיות בדף
+}
+
+//פונקית לשמירת הערכים מהטופס באובייקט המסננים
+function formObjUpdate() {
+  formObj.nameForm = searchAnimalName.value;
+  formObj.WeightForm = searchAnimalWeight.value;
+  formObj.HeightForm = searchAnimalHeight.value;
+  formObj.colorForm = colorSearch.value;
+  formObj.habitatLandForm = habitatSearchLand.checked;
+  formObj.habitatSeaForm = habitatSearchSea.checked;
+  formObj.pradatorTrueForm = pradatorSearchTrue.checked;
+  formObj.pradatorFalseForm = pradatorSearchFalse.checked;
+  localStorage.setItem("form object", JSON.stringify(formObj)); //שמירה בלוקל סטורג
+}
+
+//פונקית לניקוי הערכים מהטופס באובייקט המסננים
+function formObjClear() {
+  formObj.nameForm = "";
+  formObj.WeightForm = "";
+  formObj.HeightForm = "";
+  formObj.colorForm = "";
+  formObj.habitatLandForm = false;
+  formObj.habitatSeaForm = false;
+  formObj.pradatorTrueForm = false;
+  formObj.pradatorFalseForm = false;
+  localStorage.setItem("form object", JSON.stringify(formObj)); //שמירה בלוקל סטורג
+}
+
+//פנקציה לבניית הכרטיסים של החיות להצגה בדף
 function renderAvailableAnimals() {
-  // ממשו את הלוגיקה שמרנדרת את החיות לתוך הדיב עם האיידי animal-cards
-  // וודאו שאתם מציגים אך ורק את החיות שעומדות בפילטורים הנוכחיים
-  // במידה ואין פילטרים מסומנים, הציגו את כל החיות
+  animalCard.innerHTML = "";
+  let filteredarr = JSON.parse(localStorage.getItem("filterd array"));
+  filteredarr.forEach((animal) => {
+    animalCard.innerHTML += `
+    <div id=${animal.name} class="card">
+          <img src="./images/lion-place-holder.webp" alt="profile img" />
+          <h2>${animal.name}</h2>
+        </div>
+    
+    `;
+  });
 }
 function visitAnimal(animalName) {
   // ממשו את הלוגיקה של מעבר לעמוד חיה עבור החיה הספציפית שנבחרה
   // שמרו בלוקל סטורג' את החיה שנבחרה, כך שבעמוד החיה נוכל לשלוף אותה מהסטורג' ולהציגה בהתאם
 }
 
-function setFilter(filterKey, filterValue) {
-  /**
-   * ממשו את הלוגיקה של השמת פילטר
-   * הפילטרים הקיימים הם
-   * isPredator: true | false
-   * habitat: "land" | "sea"
-   * weight: value > user selected weight
-   * height: value > user selected height
-   * color: dropdown of all available colors
-   */
-  // ודאו כי אתם שומרים את הפילטרים שהיוזר בחר בלוקל סטורג׳ וטוענים אותם בהתאם
-  // רנדרו רק את החיות שעומדות בתנאים של הפילטרים
+function runOnCards() {
+  let cardArr = document.querySelectorAll(".card");
+  let pickedAnimal;
+  for (const card of cardArr) {
+    card.addEventListener("click", (e) => {
+      pickedAnimal = e.target.closest(".card").id;
+      localStorage.setItem("pickedAnimal", JSON.stringify(pickedAnimal));
+    });
+  }
 }
+runOnCards();
